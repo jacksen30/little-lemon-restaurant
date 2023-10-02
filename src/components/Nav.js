0e8c45717200ee027be25d051cb37e209c6ac5f4
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-10-02 12:38:03 +0800

    initalize seperate components for each main section, import into App.js

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <nav>
+        <ul>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+        </ul>
+    </nav>
+  )
+}
