fba33f76eb701b742c682c382f419ec6e9b65cf0
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-10-11 13:12:48 +0800

    add success icon to confirmation, add conditional rendering of occasion and additionalComment to show only if user entered a value, create a function to convert date from state to standard Australian format for confirmation screen

--- src/components/pages/reservations/ReservationConfirmation.js
+++ src/components/pages/reservations/ReservationConfirmation.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function ReservationConfirmation({formData, formSubmitted}) {
+export default function ReservationConfirmation({formData}) {
+
+  // Function to convert a date from YYYY-MM-DD To DD-MM-YYYY
+  const formatDateToDDMMYYYY = (dateString) => {
+    const [year, month, day] = dateString.split("-");
+    return `${day}-${month}-${year}`;
+  };
+
   return (
     <div className="flex reservation-confirmation-wrapper">
         <h1>Your Reservation<br />was Successful</h1>
+        <svg className="icon--confimation-tick" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>
         <div>
           <h2>Booking Details Below:</h2>
           <h3>Contact Details:</h3>
@@ -12,16 +20,16 @@ export default function ReservationConfirmation({formData, formSubmitted}) {
           <p>Contact Number: {formData.contactNumber}</p>
           <p>Email: {formData.email}</p>
           <h3>Reservation:</h3>
-          <p>Date: {formData.date}</p>
+          <p>Date: {formatDateToDDMMYYYY(formData.date)}</p>
           <p>Time: {formData.time}</p>
           <p>Number of Guests: {formData.guestCount}</p>
           <p>Seating Area: {formData.seatingArea}</p>
-          <p>Occasion: {formData.occasion}</p>
+          {formData.occasion && <p>Occasion: {formData.occasion}</p>}
 
-          <p className="comment">additionalComment: {formData.additionalComment}</p>
+          {formData.additionalComment && <p className="comment">Additional Info:<br /> {formData.additionalComment}</p>}
         </div>
         <div>
-          <p>A Paragraph of text explaining what to do in theevent of wrong details entered or needing toupdate the booking details at a later date aswell as any other terms or conditions the customer should be aware of in relation to online table reservation.</p>
+          <p className="ourContactDetails">If these details are incorrect, or if updates and cancellations to your reservation are required, please contact us directly at (08) 9999 0000 or via email at <a href="mailto:bookings@littlelemon.com">bookings@littlelemon.com</a></p>
           <Link class="btn--primary btn-form" to="/">Return Home</Link>
         </div>
     </div>
